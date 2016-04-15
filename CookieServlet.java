d96309c895d39054f644fb6bdd545eb226941b3f
SoichiSumi <2013s10@edu.gifu-nct.ac.jp>
2016-04-16 05:29:51 +0900

    期限テスト完了

--- CookieServlet.java
+++ CookieServlet.java
@@ -12,7 +12,7 @@ import java.util.Objects;
 //
 
 public class CookieServlet extends HttpServlet{
-
+    final int MAX_AGE=60;
     @Override
     public void init() throws ServletException{
 
@@ -53,25 +53,30 @@ public class CookieServlet extends HttpServlet{
                 out.println("<h2>CookieCount=" + visited + "</h2>");
 
                 visitedCookie.setValue(Integer.toString(visited));
+                visitedCookie.setMaxAge(MAX_AGE);
                 response.addCookie(visitedCookie);
 
             }else{
                 out.println("初回の訪問DA．他にも何かある");
                 Cookie newCookie=new Cookie("visited","1");
+                newCookie.setMaxAge(MAX_AGE);
                 response.addCookie(newCookie);
             }
         }else{
             out.println("初回の訪問DA");
             Cookie newCookie=new Cookie("visited","1");
+            newCookie.setMaxAge(MAX_AGE);
             response.addCookie(newCookie);
         }
 
         out.println("<a href=\"/hello/cookietest\">再表示</a>");
-
-        out.println("reqURI"+request.getRequestURI());
-        out.println("reqURL"+request.getRequestURL());
-        out.println("servletPath"+request.getServletPath());
-
+        out.println("<br>");
+        out.println("reqURI: "+request.getRequestURI());
+        out.println("<br>");
+        out.println("reqURL: "+request.getRequestURL());
+        out.println("<br>");
+        out.println("servletPath: "+request.getServletPath());
+        out.println("<br>");
         out.println("</body>");
         out.println("</html>");
 
